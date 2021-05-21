3316bbb381e89dd2f47309c6dea2a34aa86c6f9b
Mason Aviles <mce.aviles@gmail.com>
2021-05-21 01:47:28 -0700

    add **kwargs to base classes

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -2,12 +2,14 @@ class Band:
     booked = False
     instances = []
 
-    def __init__(self, name, members):
+    def __init__(self, name, members, **kwargs):
         # which is a string.
         self.name = name
         # which is a list of instances that inherit from Musician
         self.members = members
         self.instances.append(self)
+        for key, value in kwargs.items():
+            setattr(self, key, value)
     def __str__(self):
         return f"We are {self.name}."
 
@@ -22,8 +24,10 @@ class Band:
     
 
 class Musician:
-    def __init__(self, name):
+    def __init__(self, name, **kwargs):
         self.name = name
+        for key, value in kwargs.items():
+            setattr(self, key, value)
     def __repr__(self):
         return f'{self.__class__.__name__} instance. Name = {self.name}'
     def __str__(self):
@@ -31,7 +35,7 @@ class Musician:
 
 
 class Guitarist(Musician):
-    playing = False
+    playing_solo = False
 
     def __init__(self):
         self.guitarists = []
@@ -43,11 +47,12 @@ class Guitarist(Musician):
     def get_instrument(self):
         return 'guitar'
     def play_solo(self):
+        self.playing_solo = True
         return 'face melting guitar solo'
 
 
 class Bassist(Musician):
-    playing = False
+    playing_solo = False
 
     def __init__(self):
         self.bassists = []
@@ -59,11 +64,12 @@ class Bassist(Musician):
     def get_instrument(self):
         return 'bass'
     def play_solo(self):
+        self.playing_solo = True
         return 'bom bom buh bom'
 
 
 class Drummer(Musician):
-    playing = False
+    playing_solo = False
 
     def __init__(self):
         self.drummers = []
@@ -75,4 +81,5 @@ class Drummer(Musician):
     def get_instrument(self):
         return 'drums'
     def play_solo(self):
+        self.playing_solo = True
         return 'rattle boom crash'
