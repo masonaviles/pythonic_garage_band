73595de6617a11e985a775eab43fae39545b83e2
Mason Aviles <mce.aviles@gmail.com>
2021-05-21 01:34:04 -0700

    add play_solo, get_instrument, add to band

--- pythonic_garage_band/band.py
+++ pythonic_garage_band/band.py
@@ -1,46 +1,78 @@
 class Band:
+    booked = False
+    instances = []
+
     def __init__(self, name, members):
         # which is a string.
         self.name = name
         # which is a list of instances that inherit from Musician
         self.members = members
-    
+        self.instances.append(self)
     def __str__(self):
         return f"We are {self.name}."
+
+    def play_solos(self):
+        solos = []
+        for member in self.members:
+            solos.append(member.play_solo())
+        return tuple(solos)
+    @classmethod
+    def to_list(cls):
+        return cls.instances
+    
 
 class Musician:
     def __init__(self, name):
         self.name = name
-    
+    def __repr__(self):
+        return f'{self.__class__.__name__} instance. Name = {self.name}'
     def __str__(self):
-        return f"My name is {self.name}."
+        return f'My name is {self.name} and I play {self.get_instrument()}'
 
-class Guitarist:
+
+class Guitarist(Musician):
+    playing = False
+
     def __init__(self):
         self.guitarists = []
-    
     def __iter__(self):
         yield from self.guitarists
-    
     def add_guitarist(self, guitarist):
         self.guitarists.append(guitarist)
+    
+    def get_instrument(self):
+        return 'guitar'
+    def play_solo(self):
+        return 'face melting guitar solo'
 
-class Bassist:
+
+class Bassist(Musician):
+    playing = False
+
     def __init__(self):
         self.bassists = []
-    
     def __iter__(self):
         yield from self.bassists
-    
     def add_bassist(self, bassist):
         self.bassists.append(bassist)
 
-class Drummer:
+    def get_instrument(self):
+        return 'bass'
+    def play_solo(self):
+        return 'bom bom buh bom'
+
+
+class Drummer(Musician):
+    playing = False
+
     def __init__(self):
         self.drummers = []
-    
     def __iter__(self):
         yield from self.drummers
-    
     def add_drummer(self, drummer):
         self.drummers.append(drummer)
+
+    def get_instrument(self):
+        return 'drums'
+    def play_solo(self):
+        return 'rattle boom crash'
